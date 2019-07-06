bfadc06f822d072157aa8cc15aa5816b6016ce14
TophPT <andrebpoli.21@gmail.com>
2019-07-06 16:41:54 +0100

    url heroku

--- service/patientService.js
+++ service/patientService.js
@@ -24,9 +24,9 @@ function patientService() {
 
     function create(patient, cb) {
         request.post({
-            url:'https://sds-web-app.herokuapp.com/sds/api/v1/patient/create',
-            // url:'http://localhost:3000/sds/api/v1/patient/create',
-            form: patient
+            url:    'https://sds-web-app.herokuapp.com/sds/api/v1/patient/create',
+            // url: 'http://localhost:3000/sds/api/v1/patient/create',
+            form:   patient
         }, (error, httpResponse, body) => {
             if (error) return cb(error)
             if (httpResponse.statusCode !== 201) return cb(new Error('Este paciente não pode ser adicionado :('))
--- service/userService.js
+++ service/userService.js
@@ -13,9 +13,9 @@ function userService() {
 
     function create(data, cb) {
         request.post({
-            url:'https://sds-web-app.herokuapp.com/sds/api/v1/user/create',
-            // url:'http://localhost:3000/sds/api/v1/user/create',
-            form: data
+            url:    'https://sds-web-app.herokuapp.com/sds/api/v1/user/create',
+            // url: 'http://localhost:3000/sds/api/v1/user/create',
+            form:   data
         }, (error, httpResponse, body) => {
             if (error) return cb(error)
             if (httpResponse.statusCode !== 201) return cb(new Error ('A sua conta não foi criada com sucesso :('))
@@ -25,9 +25,9 @@ function userService() {
 
     function logIn(data, cb) {
         request.post({
-            // url:'https://sds-web-app.herokuapp.com/sds/api/v1/login',
-            url:'http://localhost:3000/sds/api/v1/login',
-            form: data
+            url:    'https://sds-web-app.herokuapp.com/sds/api/v1/login',
+            // url: 'http://localhost:3000/sds/api/v1/login',
+            form:   data
         }, (error, httpResponse, body) => {
             if (error) return cb(error)
             if (httpResponse.statusCode !== 200) return cb(new Error('Credenciais Inválidas!'))
@@ -37,9 +37,9 @@ function userService() {
 
     function findById(data, cb) {
         request.post({
-            url:'https://sds-web-app.herokuapp.com/sds/api/v1/user',
-            // url:'http://localhost:3000/sds/api/v1/user',
-            form: data
+            url:    'https://sds-web-app.herokuapp.com/sds/api/v1/user',
+            // url: 'http://localhost:3000/sds/api/v1/user',
+            form:   data
         }, (error, httpResponse, body) => {
             if (error) return cb(error)
             if (httpResponse.statusCode !== 200) return cb(new Error('Credenciais Inválidas!'))
